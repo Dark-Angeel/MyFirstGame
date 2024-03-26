20623f38aa68562cd87215a6d5025717ef5d00f8
Dark-Angeel <133654202+Dark-Angeel@users.noreply.github.com>
2024-03-26 12:47:38 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigiBodyComponent;
+    bool jump;
+    bool touchGround;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigiBodyComponent = GetComponent<Rigidbody>();
+        jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && touchGround == true)
+        {
+            jump = true;
+        }
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            rigiBodyComponent.AddForce(3 * Vector3.up, ForceMode.VelocityChange);
+            jump = false;
+            touchGround = false;
+        }
+        rigiBodyComponent.velocity = new Vector3(horizontal, rigiBodyComponent.velocity.y, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        touchGround= true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
